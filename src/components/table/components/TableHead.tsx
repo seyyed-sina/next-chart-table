fd264ea8f8e1ce4f0e2d2a623cb7adb9441a8d4b
sina <cna_fp@yahoo.com>
2024-10-09 14:17:54 +0330

    change: sort icon

--- src/components/table/components/TableHead.tsx
+++ src/components/table/components/TableHead.tsx
@@ -44,7 +44,8 @@ export const TableHead = ({ table }: TableHeadProps) => {
               {header.isPlaceholder ? null : (
                 <div
                   className={clx(
-                    header.column.getCanSort() && 'cursor-pointer select-none',
+                    header.column.getCanSort() &&
+                      'cursor-pointer flex items-center gap-1 select-none',
                   )}
                   role="button"
                   title={getSortLabel(header)}
@@ -54,8 +55,44 @@ export const TableHead = ({ table }: TableHeadProps) => {
                     header.getContext(),
                   )}
                   {{
-                    asc: ' 🔼',
-                    desc: ' 🔽',
+                    asc: (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="size-5 stroke-primary-100"
+                        width="48"
+                        height="48"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M4 6l7 0" />
+                        <path d="M4 12l7 0" />
+                        <path d="M4 18l9 0" />
+                        <path d="M15 9l3 -3l3 3" />
+                        <path d="M18 6l0 12" />
+                      </svg>
+                    ),
+                    desc: (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="size-5 stroke-primary-100"
+                        width="48"
+                        height="48"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M4 6l9 0" />
+                        <path d="M4 12l7 0" />
+                        <path d="M4 18l7 0" />
+                        <path d="M15 15l3 3l3 -3" />
+                        <path d="M18 6l0 12" />
+                      </svg>
+                    ),
                   }[header.column.getIsSorted() as string] ?? null}
                 </div>
               )}
