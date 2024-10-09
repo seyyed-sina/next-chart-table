49e5a31c22011214ddee90942f68b85ca5f7e97f
sina <cna_fp@yahoo.com>
2024-10-09 12:24:12 +0330

    move font definition outside layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
 import { MainLayout } from '@/components/layout/MainLayout';
+import { inter } from '@/constants/font';
 import './globals.css';
-
-export const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-});
 
 export const metadata: Metadata = {
   title: 'Next App Chart - Table demo',
